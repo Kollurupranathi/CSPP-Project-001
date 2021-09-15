8f154f501c9d7b480aeb4208c4ea50681a5ab134
Kollurupranathi <kpranathi009@gmail.com>
2021-09-15 12:04:33 +0530

    changes commands

--- curl.py
+++ curl.py
@@ -1,16 +1,28 @@
 """The python code helps to read the command line input for curl method."""
-
+#the first line is a comment
 import sys
+#importing sys library using "import" keyword
 from lib.helper import curl
-
+#Importing "curl" functions from helper library using "from" "import" keywords
 URL = None
+#URL is a variable which is initialized with none
 ARG_CNT = len(sys.argv) - 1
+#length is a function containing a predefined argument "sys.argv" and it is used to length of the file
+#ARG_CNT is a variable assigned with len(sys.argv) minus one
 
 if ARG_CNT != 1:
     print('Usage: curl [URL]...')
-
+#"if" is a conditional statement used to perform multiple outcomes of a previous step
+# if the value of ARG_CNT varialble is not equal to one, the condition will be true. This makes to execute the next line
+#print is a function used to print the data given in it
+#Here, it print a string "Usage: curl [URL]..."
 if ARG_CNT == 1:
     URL = sys.argv[1]
     if 'http' not in URL[:5]:
         URL = "http://"+URL
     print(curl(URL))
+# if the value of ARG_CNT variable is equal to one, the condition will be true. This makes to execute the next line
+#sys.argv[1] is stored in a variable named "URL"
+# if an URL does not have 'http' in it 
+# then overwrite the URL variable by adding 'http' to the existing URL variable
+#print the function curl containing URL as an argument
--- wc.py
+++ wc.py
@@ -1,18 +1,28 @@
 """Implementing the wc shell command in python."""
-
+#the first line is a comment
 import sys
-
+#importing sys library using "import" keyword
 from lib.helper import wc, readfile
-
+#Importing "cat" "readfile" functions from helper library using "from" "import" keywords
 TEXT = None
+#TEXT is a variable which is initialized with none
 ARG_CNT = len(sys.argv) - 1
-
+#length is a function containing a predefined argument "sys.argv" and it is used to length of the file
+#ARG_CNT is a variable assigned with len(sys.argv) minus one
 if ARG_CNT == 0:
     TEXT = sys.stdin.read()
-
+#"if" is a conditional statement used to perform multiple outcomes of a previous step
+# if the value of ARG_CNT varialble is zero, the condition will be true. 
+# This makes to execute the next line 
+#As the condition is true, sys.stdin.read() is stored in a variable named "TEXT"
 if ARG_CNT == 1:
     filename = sys.argv[1]
     TEXT = readfile(filename)
-
+#if the value of ARG_CNT variable is one, the condition will be true. 
+#This makes to execute the next line
+#As the condition is true, sys.argv[1] is stored in a variable named "filename" 
+# read(filename) is stored in a variable named "TEXT"
 response = wc(TEXT)
+#we're creating a "response" variable and saving the function wc, which will return the value which has an argument "TEXT"
 print(" " + str(response[0]) + "  " + str(response[1]) + " " + str(response[2]))
+#all the strings are added with spacing between them and printed
